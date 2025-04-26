1935b627332f78994fab797b37f24c9e5a885156
Hennadii Kolomoiets <kgp@znu.edu.ua>
2025-04-26 22:45:42 +0300

    Make Billing as subclass of Shipping.

--- src/main/java/ua/edu/znu/wcrestapp/model/Billing.java
+++ src/main/java/ua/edu/znu/wcrestapp/model/Billing.java
@@ -3,18 +3,26 @@ package ua.edu.znu.wcrestapp.model;
 import lombok.*;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Billing {
-    private String first_name;
-    private String last_name;
-    private String company;
-    private String address_1;
-    private String address_2;
-    private String city;
-    private String state;
-    private String postcode;
-    private String country;
+public class Billing extends Shipping {
+
+    @Builder(builderMethodName = "billingBuilder")
+    public Billing(String firstName, String lastName, String company, String address1, String address2,
+                   String city, String state, String postcode, String country, String email, String phone) {
+        super(firstName, lastName, company, address1, address2, city, state, postcode, country);
+        this.email = email;
+        this.phone = phone;
+    }
+
     private String email;
     private String phone;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Billing billing)) return false;
+        if (!super.equals(o)) return false;
+
+        if (getEmail() != null ? !getEmail().equals(billing.getEmail()) : billing.getEmail() != null) return false;
+        return getPhone() != null ? getPhone().equals(billing.getPhone()) : billing.getPhone() == null;
+    }
 }
--- src/main/java/ua/edu/znu/wcrestapp/model/Shipping.java
+++ src/main/java/ua/edu/znu/wcrestapp/model/Shipping.java
@@ -1,15 +1,17 @@
 package ua.edu.znu.wcrestapp.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Shipping {
     private String first_name;
-    private String  last_name;
+    private String last_name;
     private String company;
     private String address_1;
     private String address_2;
